6063ef03117e46d97095d0206d769ec425160060
Kumar Priyanshu Raj <kumarpriyanshuraj@csus.edu>
2023-04-13 10:02:55 -0700

    Added getNominiesByYear in the server.js file

--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 //use require instead of import
 import mongoose from 'mongoose';
 import { mongodbcredentials } from './secret.js';
-
 const connectionString = 'mongodb+srv://' + mongodbcredentials + '.nkgfuzj.mongodb.net/?retryWrites=true&w=majority';
+const OscarNominiesSchema = new mongoose.Schema({
+	year: String,
+	category: String,
+	winner: String,
+	entity: String,
+});
 
-// Define the Mongoose schema for the Oscar data
+
 const OscarWinnerSchema = new mongoose.Schema({
 	year_film: String,
 	year_cermony: String,
@@ -18,6 +23,7 @@ const OscarWinnerSchema = new mongoose.Schema({
 
 // Create a Mongoose model for the Oscar data
 const OscarWinner = mongoose.model('Oscar_Winners', OscarWinnerSchema, 'the_oscar_award');
+const OscarNominies = mongoose.model('Oscar_Nominies', OscarNominiesSchema, 'oscar_nominies');
 
 async function getWinnerByYear(year) {
 	try{
@@ -37,7 +43,7 @@ async function getWinnerByYear(year) {
 async function getWinnerByActor(actor) {
 	try {
 	  await mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-	  const actors = await OscarWinner.find({ name: actor, winner: "True" }, { name: 1, _id: 0 });
+	  const actors = await OscarWinner.find({ name: actor, winner: "True", category: "ACTOR" }, { name: 1, _id: 0 });
 	  return actors;
 	} catch (err) {
 	  console.log('Error finding winners by actor:', err);
@@ -46,12 +52,32 @@ async function getWinnerByActor(actor) {
 	  await mongoose.disconnect();
 	}
   }
- 
+async function getNominesByYear(year) {
+	try{
+	await mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+	const nominies = await OscarNominies.find({year: year, winner: "True", category: "ACTRESS"}, {entity: 1, _id: 0});
+	return nominies;
+	}
+	catch(err){
+		console.log('Error finding nominies by year:', err);
+		return [];
+	}
+	finally{
+		await mongoose.disconnect();
+	}}
+
+
+
+
+
+
+
 
 //call the function
-getWinnerByYear('2015').then(winners => console.log(winners));
-getWinnerByActor('Janet Gaynor').then(actors => console.log(actors));
-
+// getWinnerByYear('2015').then(winners => console.log(winners));
+// getWinnerByActor('Janet Gaynor').then(actors => console.log(actors));
+// getNominesByYear('2022').then(nominies => console.log(nominies));
+getNominesByYear('1927').then(nominies => console.log(nominies));
 
 
 
